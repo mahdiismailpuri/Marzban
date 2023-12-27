41299e1ebb643cef983a2ee10f365f7287637b96
mahdiismailpuri <135647913+mahdiismailpuri@users.noreply.github.com>
2023-12-27 19:23:25 +0300

    __init__.py

--- app/telegram/__init__.py
+++ app/telegram/__init__.py
@@ -6,26 +6,41 @@ from app import app
 from telebot import TeleBot, apihelper
 
 
-bot = None
-if TELEGRAM_API_TOKEN:
-    apihelper.proxy = {'http': TELEGRAM_PROXY_URL, 'https': TELEGRAM_PROXY_URL}
-    bot = TeleBot(TELEGRAM_API_TOKEN)
+def load_handlers():
+    handler_dir = dirname(__file__) + "/handlers/"
+    for name in handler_names:
+        spec = importlib.util.spec_from_file_location(name, f"{handler_dir}{name}.py")
+        spec.loader.exec_module(importlib.util.module_from_spec(spec))
 
-handler_names = ["admin", "report", "user"]
 
 @app.on_event("startup")
 def start_bot():
-    if bot:
-        handler_dir = dirname(__file__) + "/handlers/"
-        for name in handler_names:
-            spec = importlib.util.spec_from_file_location(name, f"{handler_dir}{name}.py")
-            spec.loader.exec_module(importlib.util.module_from_spec(spec))
+    if TELEGRAM_API_TOKEN:
+        bot = TeleBot(TELEGRAM_API_TOKEN)
 
-        from app.telegram import utils # setup custom handlers
-        utils.setup()
+    handler_thread = Thread(target=load_handlers)
+    handler_thread.start()
 
-        thread = Thread(target=bot.infinity_polling, daemon=True)
-        thread.start()
+    from app.telegram import utils  # setup custom handlers
+    utils.setup()
+
+    thread = Thread(target=bot.infinity_polling, daemon=True)
+    thread.start()
+
+
+def send_status_change_report(bot, message, user_id, message_id, new_status):
+    report_message = f"User {user_id} status changed to {new_status}"
+    bot.send_message(message.chat.id, report_message, message_id)
+
+
+def report_status_change(bot, message, user_id, message_id, new_status):
+    send_status_change_report(bot, message, user_id, message_id, new_status)
+
+    with apihelper.proxy_from_url(TELEGRAM_PROXY_URL):
+        if new_status == "deleted":
+            report_user_deletion(bot, message, user_id, message_id)
+        else:
+            report_user_subscription_revoked(bot, message, user_id, message_id)
 
 
 from .handlers.report import (  # noqa
